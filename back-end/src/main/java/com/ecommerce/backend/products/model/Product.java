8a3e9adb9b526a60b8f71abe09fdaae756478712
NicolasDesnoust <desnoust.nicolas451@gmail.com>
2020-10-17 21:49:23 +0200

    Mise à jour du javabean Produit [BCKND]

--- back-end/src/main/java/com/ecommerce/backend/products/model/Product.java
+++ back-end/src/main/java/com/ecommerce/backend/products/model/Product.java
@@ -1,16 +1,39 @@
 package com.ecommerce.backend.products.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
-public class Product {
+@Entity
+@Table(name = "products")
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
 	private int id;
+	
 	private String title;
+	
 	private String image;
+	
 	private String images;
+	
 	private String description;
+	
+	@Column(name = "short_desc")
 	private String shortDescription;
+	
 	private double price;
+	
 	private int quantity;
+	
+	@Column(name = "cat_id")
 	private int categoryId;
 }
