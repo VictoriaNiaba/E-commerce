3198f3f91f2993530730f5c7023dfebcff606b1f
NicolasDesnoust <desnoust.nicolas451@gmail.com>
2021-01-03 20:30:09 +0100

    Fix: désactivation de l'export automatique des repos. [BCKND]

--- back-end/src/main/java/com/ecommerce/backend/users/services/UserRepository.java
+++ back-end/src/main/java/com/ecommerce/backend/users/services/UserRepository.java
@@ -2,10 +2,11 @@ package com.ecommerce.backend.users.services;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.ecommerce.backend.users.model.User;
 
-@Repository
+@Repository @RepositoryRestResource(exported = false)
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 }
--- back-end/src/main/java/com/ecommerce/backend/users/services/UserSearchRepository.java
+++ back-end/src/main/java/com/ecommerce/backend/users/services/UserSearchRepository.java
@@ -1,10 +1,11 @@
 package com.ecommerce.backend.users.services;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.ecommerce.backend.users.model.SearchableUser;
 
-@Repository
+@Repository @RepositoryRestResource(exported = false)
 public interface UserSearchRepository extends ElasticsearchRepository<SearchableUser, Long> {
 }
