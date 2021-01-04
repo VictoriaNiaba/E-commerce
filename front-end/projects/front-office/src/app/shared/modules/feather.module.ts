e865ec71276384f60266c5278e2416873148682d
NicolasDesnoust <desnoust.nicolas451@gmail.com>
2021-01-04 19:30:37 +0100

    Configuration d'un module exportant les icônes Feather [BCKND]

---
+++ front-end/projects/front-office/src/app/shared/modules/feather.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { FeatherModule as AngularFeatherModule } from 'angular-feather';
+
+/* --------------- Usage : <i-feather name="eye"></i-feather> --------------- */
+
+import { ShoppingCart, User, X } from 'angular-feather/icons';
+
+// Select some icons (use an object, not an array)
+const icons = {
+  ShoppingCart,
+  User,
+  X
+};
+
+@NgModule({
+  imports: [AngularFeatherModule.pick(icons)],
+  exports: [AngularFeatherModule],
+})
+export class FeatherModule {}
--- front-end/projects/front-office/src/app/shared/shared.module.ts
+++ front-end/projects/front-office/src/app/shared/shared.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './modules/material.module';
 import { RouterModule } from '@angular/router';
+import { FeatherModule } from './modules/feather.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     RouterModule,
-    MaterialModule
+    MaterialModule,
+    FeatherModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     CommonModule,
     RouterModule,
-    MaterialModule
+    MaterialModule,
+    FeatherModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
