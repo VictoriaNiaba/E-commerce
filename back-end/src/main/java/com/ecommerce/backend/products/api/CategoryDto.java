7d388aa822606b5549591985aa762ad79c1a8f7f
Victoria <niaba.victoria@gmail.com>
2021-01-09 17:40:39 +0100

    FIx(categories route): ajout des ids des produits à la réponse [BCKND]

--- back-end/src/main/java/com/ecommerce/backend/products/api/CategoryDto.java
+++ back-end/src/main/java/com/ecommerce/backend/products/api/CategoryDto.java
@@ -1,6 +1,7 @@
 package com.ecommerce.backend.products.api;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
@@ -8,23 +9,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data @Builder 
-@NoArgsConstructor @AllArgsConstructor
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@Schema(
-		description = "Unique identifier of the category.",
-		example = "2",
-		required = true
-	)
+	@Schema(description = "Unique identifier of the category.", example = "2", required = true)
 	private Long id;
-	
-	@Schema(
-		description = "Title of the category.",
-		example = "Shoes",
-		required = true
-	)
+
+	@Schema(description = "Title of the category.", example = "Shoes", required = true)
 	private String title;
+
+	private Collection<Long> productIds;
 }
--- back-end/src/main/java/com/ecommerce/backend/products/services/CategoryMapper.java
+++ back-end/src/main/java/com/ecommerce/backend/products/services/CategoryMapper.java
@@ -2,12 +2,19 @@ package com.ecommerce.backend.products.services;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.ecommerce.backend.products.api.CategoryDto;
 import com.ecommerce.backend.products.model.Category;
+import com.ecommerce.backend.products.model.Product;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface CategoryMapper {
-    
+
+	@Mapping(source = "products", target = "productIds")
 	public CategoryDto toDto(Category category);
+
+	default Long productToProductId(Product product) {
+		return product.getId();
+	}
 }
